47250c4426a5730574aca567fc747cb6ab3714f6
user2695 <53275443+user2695@users.noreply.github.com>
2021-07-16 11:49:01 +0530

    version1.2

--- server.js
+++ server.js
@@ -1,9 +1,41 @@
 //jshint esversion:6
 
 const express = require("express");
-const bodyParser = require("body-parser");
-const { check, validationResult} = require("express-validator");
 const app = express();
+
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const DB = "mongodb+srv://admin-zubair:zubair12@cluster0.7tjmi.mongodb.net/testDatabase?retryWrites=true&w=majority/notesDB"
+
+mongoose.connect(DB, {
+ useNewUrlParser: true,
+ useUnifiedTopology: true,
+ useCreateIndex: true,
+ useFindAndModify: false
+}).then(()=>{
+    console.log("connected to mongoDb Atlas");
+}).catch((error)=>
+        console.log("no connection"));
+
+//Schema
+
+const notesSchema = {
+    fullname: String,
+    email: String,
+    psw: String,
+    telcollector: String,
+    org:String,
+    orgEmail: String
+}
+
+const Note = mongoose.model("Note",notesSchema);
+
+
+
+
+
+const { check, validationResult} = require("express-validator");
+
 app.use("/styles",express.static("styles"));
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -25,6 +57,23 @@ app.get("/login",function(req,res){
 app.get("/register", function(req, res){
     res.render("register")
   })
+
+
+
+  app.post("/", function(req, res){
+    let newNote = new Note({
+        fullname:req.body.fullname,
+        email:req.body.email,
+        psw:req.body.psw,
+        telcollector:req.body.telcollector,
+        org:req.body.org,
+        orgEmail:req.body.orgEmail
+    });
+    newNote.save();
+    res.redirect("/");
+})
+
+
 
 
 app.listen(3000, function(){
